23b51ce4ff1918470b1d68313d6969a8bf333fb3
sourabh <sourabhrahafbg@gmail.com>
2021-05-17 01:23:45 +0530

    learning gitignore

---
+++ text.cpp
@@ -0,0 +1,17 @@
+// Sourabh Raha
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL)
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+ 
+int main()
+{
+    fastio;
+    ll test_cases;
+    cin>>test_cases;
+    while(test_cases--)
+    {
+          cout<<"Hello";
+    }
+    return 0;
+}
